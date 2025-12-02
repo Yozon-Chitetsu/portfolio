d8fa54e10cbb3c5b0d950d240f0d57b117deb16c
Muhammad-Aqeel-2226 <0302muhammadaqeel389@gmail.com>
2025-12-02 20:57:27 +0500

    fixed component name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
-import Header from "./components/header";
+import Header from "./components/Header";
 
 function App() {
   return (
